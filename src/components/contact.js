6a9ca1a1fca56543f6b4b526c54192ae54e8715d
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-06-02 08:49:41 +0530

    contetn update

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,7 +7,12 @@ export const contact = `
 <a href="/contact" data-navigo>Contact</a>
 </div>
 <div class="content">
-<h1>This is Contact</h1>
+<h1 class="head">Get in touch.</h1>
+<h1>Gmail</h1>
+<h1>Instagram</h1>
+<h1>Twitter</h1>
+<h1>Github</h1>
+<h1>LinkedIn</h1>
 </div>
 </div>
 `
