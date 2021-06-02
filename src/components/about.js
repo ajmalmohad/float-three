b0d2dfef353df7c9e6ea69b62fca91cfd784fad0
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-06-02 12:14:12 +0530

    about page update

--- src/components/about.js
+++ src/components/about.js
@@ -8,22 +8,30 @@ export const about = `
 </div>
 <div class="content">
 <div class="info">
+<div class="head">
+<h1>We are based on</h1>
+<h1>kerala, india.</h1>
+</div>
 <div class="sec1">
-<h1>We are based on kerala, india.</h1>
-<h1><a href="https://instagram.com/ajmal_mohad" target="_blank" >Founder - Ajmal Mohad.</a></h1>
+<p>We create neat animated and interactive websites. 
+Your websites will be fast, interactive and user friendly. 
+We will help you to build your brand or personal brand by creating
+awesome designs.</p>
+<p>We can help you build cool websites with nice ui effects from scratch or 
+we can add some effects to your exisiting websites</p>
 </div>
 <div class="sec2">
-<h1>The purpose of this company is to satisfy our clients with awesome websites
+<p>The purpose of this company is to satisfy our clients with awesome websites
 with cool animations and 3d elements along with high performance. 
 Our primary focus is on great design with great performance. No need to worry about
- Webpage optimizations and SEO. We will ensure your website loads fast and looks cool.</h1>
+ Webpage optimizations and SEO. We will ensure your website loads fast and looks cool.</p>
 </div>
 <div class="sec3">
 <h1>Why choose us?</h1>
-<h1>- we’re easy to work with.</h1>
-<h1>- we’ll understand your brand.</h1>
-<h1>- we don’t cost the whole earth.</h1>
-<h1>- we’re professionals.</h1>
+<p>. we’re easy to work with.</p>
+<p>. we’ll understand your brand.</p>
+<p>. we don’t cost the whole earth.</p>
+<p>. we’re professionals.</p>
 </div>
 </div>
 </div>
